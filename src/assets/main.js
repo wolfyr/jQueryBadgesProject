9451d0b644f45a92a9816d46c263b7302460b3c2
wolfyr <35544104+wolfyr@users.noreply.github.com>
2018-02-03 15:27:57 +0100

    change

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,40 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+   url: 'https://www.codeschool.com/users/4501757.json',
+   dataType: 'jsonp',
+   success: function(response) {
+     // handle response
+     addCourses(response.courses.completed);
+   }
+ });
+
+function addCourses(courses){
+
+var $badges = $('#badges');
+
+  courses.forEach(function(course){
+    var $course = $('<div />', {
+      'class': 'course'
+    }).appendTo($badges);
+
+$('<h3 />', {
+  text: course.title
+}).appendTo($course);
+
+$('<img />',{
+ scr: course.badge
+}).appendTo($course);
+
+$('<a />', {
+  'class': 'btn btn-primary',
+  target: '_blank',
+  href: course.url,
+  text: 'See Course'
+}).appendTo($course);
+
+  })
+}
 
 });
